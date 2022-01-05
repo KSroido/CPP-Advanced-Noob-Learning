64f0d042be318b572256429e17119143122ac995
KSroido <ksxmyqj@gmail.com>
2022-01-05 10:03:40 +0800

    1

---
+++ i++and++i/++.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    int i = 0;
+    int j = 5;
+    i++ = j;
+    ++i = j;
+
+    //go to see gcc's complain
+
+
+}
---
+++ i++and++i/++.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+
+
+    int i = 0;
+    int j = 5;
+    i++ = j;
+    ++i = j;
+    //go to see g++'s complain
+}
