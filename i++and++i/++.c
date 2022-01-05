2b104740d40441b32b1fe2683193846ed8171ab1
KSroido <ksxmyqj@gmail.com>
2022-01-05 10:03:57 +0800

    1

--- i++and++i/++.c
+++ i++and++i/++.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 int main(){
-
     int i = 0;
     int j = 5;
     i++ = j;
     ++i = j;
-
     //go to see gcc's complain
-
-
 }
--- i++and++i/++.cpp
+++ i++and++i/++.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 int main(){
-
-
     int i = 0;
     int j = 5;
     i++ = j;
